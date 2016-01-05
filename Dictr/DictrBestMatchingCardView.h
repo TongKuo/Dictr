a00737a6872d5f46d744ef3ca4e74cefc6cb6972
Tong Kuo <contact@tong-kuo.me>
2016-01-05 20:49:46 +0800

    Introduced DictrBestMatchingCardView and its controller class

---
+++ Dictr/DictrBestMatchingCardView.h
@@ -0,0 +1,14 @@
+//
+//  DictrBestMatchingCardView.h
+//  Dictr
+//
+//  Created by Tong G. on 1/5/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+@import Cocoa;
+
+// DictrBestMatchingCardView class
+@interface DictrBestMatchingCardView : NSView
+
+@end // DictrBestMatchingCardView class
---
+++ Dictr/DictrBestMatchingCardViewController.h
@@ -0,0 +1,14 @@
+//
+//  DictrBestMatchingCardViewController.h
+//  Dictr
+//
+//  Created by Tong G. on 1/5/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+@import Cocoa;
+
+// DictrBestMatchingCardViewController class
+@interface DictrBestMatchingCardViewController : NSViewController
+
+@end // DictrBestMatchingCardViewController class
