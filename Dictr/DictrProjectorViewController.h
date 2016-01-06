e30879eac71665259bef8437b17bd2afc5a5cfbb
Tong Kuo <contact@tong-kuo.me>
2016-01-07 01:03:15 +0800

    Introduced DictrProjectorViewController class

---
+++ Dictr/DictrProjectorViewController.h
@@ -0,0 +1,14 @@
+//
+//  DictrProjectorViewController.h
+//  Dictr
+//
+//  Created by Tong G. on 1/7/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+@import Cocoa;
+
+// DictrProjectorViewController class
+@interface DictrProjectorViewController : NSViewController
+
+@end // DictrProjectorViewController class
---
+++ Dictr/DictrProjectorViewController.m
@@ -0,0 +1,27 @@
+//
+//  DictrProjectorViewController.m
+//  Dictr
+//
+//  Created by Tong G. on 1/7/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import "DictrProjectorViewController.h"
+
+// Private Interfaces
+@interface DictrProjectorViewController ()
+
+@end // Private Interfaces
+
+// DictrProjectorViewController class
+@implementation DictrProjectorViewController
+
+#pragma mark - Initializations
+
+- ( void ) viewDidLoad
+    {
+    [ super viewDidLoad ];
+    // Do view setup here.
+    }
+
+@end // DictrProjectorViewController class
