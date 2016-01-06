629db63202d1e9e2fa5bb1273a750b28bcb621a5
Tong Kuo <contact@tong-kuo.me>
2016-01-06 21:44:52 +0800

    Introduced DictrCambridgeTranslatorType enums

--- Dictr/DictrTranslator.h
+++ Dictr/DictrTranslator.h
@@ -8,8 +8,29 @@
 
 @import Foundation;
 
+typedef NS_ENUM( NSUInteger, DictrCambridgeTranslatorType )
+    { DictrCambridgeEnglish2English
+    , DictrCambridgeEnglish2Arabic
+    , DictrCambridgeEnglish2Catalan
+    , DictrCambridgeEnglish2Italian
+    , DictrCambridgeEnglish2Japanese
+    , DictrCambridgeEnglish2Korean
+    , DictrCambridgeEnglish2Portuguese
+    , DictrCambridgeEnglish2Polish
+    , DictrCambridgeEnglish2Russian
+    , DictrCambridgeEnglish2SimplifiedChinese
+    , DictrCambridgeEnglish2TranditionalChinese
+    };
+
 // DictrTranslator class
 @interface DictrTranslator : NSObject <NSTextFieldDelegate>
+    {
+@private
+    NSURL* __apiBaseURL;
+    }
+
+@property ( strong, readwrite ) NSString* accessKey;
+@property ( assign, readwrite ) DictrCambridgeTranslatorType translatorType;
 
 #pragma mark - Initializations
 
--- Dictr/DictrTranslator.m
+++ Dictr/DictrTranslator.m
@@ -8,6 +8,13 @@
 
 #import "DictrTranslator.h"
 
+// Private Interfaces
+@interface DictrTranslator ()
+
+@property ( strong, readonly ) NSString* __dictCode;
+
+@end // Private Interfaces
+
 // DictrTranslator class
 @implementation DictrTranslator
 
@@ -22,8 +29,15 @@ DictrTranslator static* sDefaultTranslator;
 - ( instancetype ) init
     {
     if ( !sDefaultTranslator )
+        {
         if ( self = [ super init ] )
+            {
+            self->__apiBaseURL = [ NSURL URLWithString: @"https://dictionary.cambridge.org/api/v1/" ];
+            self.translatorType = DictrCambridgeEnglish2English;
+
             sDefaultTranslator = self;
+            }
+        }
 
     return sDefaultTranslator;
     }
@@ -33,6 +47,51 @@ DictrTranslator static* sDefaultTranslator;
 - ( void ) controlTextDidBeginEditing: ( NSNotification* )_Notif
     {
     NSLog( @"%@", _Notif );
+    }
+
+#pragma mark - Private Interfaces
+
+- ( NSString* ) __dictCode
+    {
+    NSString* dictCode = nil;
+
+    switch ( self.translatorType )
+        {
+        // Cambridge Advanced Learner's Dictionary
+        case DictrCambridgeEnglish2English: dictCode = @"british"; break;
+
+        // English-Arabic Dictionary
+        case DictrCambridgeEnglish2Arabic: dictCode = @"english-arabic"; break;
+
+        // English-Catalan Dictionary
+        case DictrCambridgeEnglish2Catalan: dictCode = @"english-catalan"; break;
+
+        // English-Italian Dictionary
+        case DictrCambridgeEnglish2Italian: dictCode = @"english-italian"; break;
+
+        // English-Japanese Dictionary
+        case DictrCambridgeEnglish2Japanese: dictCode = @"english-japanese"; break;
+
+        // English-Korean Dictionary
+        case DictrCambridgeEnglish2Korean: dictCode = @"english-korean"; break;
+
+        // English-Portuguese Dictionary
+        case DictrCambridgeEnglish2Portuguese: dictCode = @"english-portuguese"; break;
+
+        // Cambridge Learner's Dictionary English-Polish
+        case DictrCambridgeEnglish2Polish: dictCode = @"english-polish"; break;
+
+        // Cambridge Learner's Dictionary English-Russian
+        case DictrCambridgeEnglish2Russian: dictCode = @"english-russian"; break;
+
+        // Cambridge Advanced Learner's Dictionary English-Chinese (Simplified)
+        case DictrCambridgeEnglish2SimplifiedChinese: dictCode = @"english-chinese-simplified"; break;
+
+        // Cambridge Advanced Learner's Dictionary English-Chinese (Traditional)
+        case DictrCambridgeEnglish2TranditionalChinese: dictCode = @"english-chinese-traditional"; break;
+        }
+
+    return dictCode;
     }
 
 @end // DictrTranslator class
