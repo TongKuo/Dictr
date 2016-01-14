c124e2fd9aa5e358de32c1518fe9699100f458aa
Tong Kuo <contact@tong-kuo.me>
2016-01-14 18:34:08 +0800

    Back fucking up

---
+++ Dictr/vendors/DictrSenseBlock.h
@@ -0,0 +1,14 @@
+//
+//  DictrSenseBlock.h
+//  Dictr
+//
+//  Created by Tong G. on 1/14/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import "DictrXMLObject.h"
+
+// DictrSenseBlock class
+@interface DictrSenseBlock : DictrXMLObject
+
+@end // DictrSenseBlock class
---
+++ Dictr/vendors/DictrSenseBlock.m
@@ -0,0 +1,24 @@
+//
+//  DictrSenseBlock.m
+//  Dictr
+//
+//  Created by Tong G. on 1/14/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import "DictrSenseBlock.h"
+
+// DictrSenseBlock class
+@implementation DictrSenseBlock
+
+#pragma mark - Initializations
+
+- ( instancetype ) initWithXML: ( NSXMLNode* )_XMLNode
+    {
+    if ( self = [ super initWithXML: _XMLNode ] )
+        ;
+
+    return self;
+    }
+
+@end // DictrSenseBlock class
